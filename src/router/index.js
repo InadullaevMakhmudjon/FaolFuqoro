a6507eca21688ae34be0fd58c1c495cf4ba7232f
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-08 17:43:59 +0500

    Routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../views/Login.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+    props: (route) => ({ ...route.params }),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  // mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+export default router;
