142d2956325198758c05246c2938aa2efbee6ec4
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-05-24 18:12:44 +0500

    Custom prisma client created

--- prisma/generated/prisma-client/index.js
+++ prisma/generated/prisma-client/index.js
@@ -1,41 +1,41 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_lib_1 = require("prisma-client-lib");
-var typeDefs = require("./prisma-schema").typeDefs;
 
-var models = [
+Object.defineProperty(exports, '__esModule', { value: true });
+const prisma_lib_1 = require('prisma-client-lib');
+const { typeDefs } = require('./prisma-schema');
+
+const models = [
   {
-    name: "Role",
-    embedded: false
+    name: 'Role',
+    embedded: false,
   },
   {
-    name: "Status",
-    embedded: false
+    name: 'Status',
+    embedded: false,
   },
   {
-    name: "People",
-    embedded: false
+    name: 'People',
+    embedded: false,
   },
   {
-    name: "ReportType",
-    embedded: false
+    name: 'ReportType',
+    embedded: false,
   },
   {
-    name: "User",
-    embedded: false
+    name: 'User',
+    embedded: false,
   },
   {
-    name: "Report",
-    embedded: false
+    name: 'Report',
+    embedded: false,
   },
   {
-    name: "ReportComment",
-    embedded: false
-  }
+    name: 'ReportComment',
+    embedded: false,
+  },
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466`
+  endpoint: 'http://localhost:4466',
 });
 exports.prisma = new exports.Prisma();
