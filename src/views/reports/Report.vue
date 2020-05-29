6c21fe828c49d701c14521fb9c55438793fa6285
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-05-29 08:20:24 +0500

    Report design changed

--- src/views/reports/Report.vue
+++ src/views/reports/Report.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
  v-container(v-if="report")
+     GmapMap(
+        :center="{ lat: report.lat, lng: report.lng+0.02 }"
+        :zoom="14"
+        :options="options"
+     ).new-map
+          GmapMarker(:position="{ ...report }" :icon="`http://maps.google.com/mapfiles/ms/icons/${status.color}-dot.png`")
      v-dialog(v-model="open" width="500")
       v-card
         v-container.pa-5
@@ -18,20 +24,32 @@
        v-if="button.show"
        :key="i"
        @click="button.callBack"
-       outlined
-      ).ma-2 {{ button.name }}
+       :color="button.color"
+      ).white--text.ma-2 {{ button.name }}
      v-row
-         v-col(cols="6")
-             div(:style="`background-image: url('${report.image}')`").custom-container
-                 .comment
-                     h1(align="center") {{ comment ? comment.comment : '' }}
-         v-col(cols="6")
-             GmapMap(
-                 :center="{ ...report }"
-                 :zoom="14"
-                 :options="options"
-             ).map
-               GmapMarker(:position="{ ...report }" :icon="`http://maps.google.com/mapfiles/ms/icons/${status.color}-dot.png`")
+        v-col(cols="4" ).paper-background
+          div(style="background: white; overflow: hidden; border-radius: 5px")
+            div(:style="`background-image: url('${report.image}')`").custom-container
+            div(style="padding: 8px")
+              v-row
+                v-col(cols="4")
+                  div(style="display: flex; align-items: center; justify-content: center; height: 100%")
+                    img(style="width: 100px; height: 100px" :src="reportDetails.creatorImage")
+                v-col(cols="8")
+                  div(style="display: flex; flex-direction: column")
+                    span {{ `Nomer: ${reportDetails.number}` }}
+                    span {{ `Kimdan: ${reportDetails.creatorName}` }}
+                    span {{ `Telifon: ${reportDetails.creatorPhone}` }}
+                    span {{ `Vaqti: ${reportDetails.reportDate}` }}
+                    span {{ `Gacha: ${reportDetails.reportDeadline}` }}
+          v-row.pa-2
+            div(
+              v-for="(item, index) in reportDetails.comments"
+              :style="`background: ${index === 0 ? 'lightgreen' : 'lightblue'}`"
+            ).custom-comment
+              h4.text-center {{ `${item.status} - ${item.date}` }}
+              span {{ item.comment }}
+
 </template>
 <script>
 import REPORT from '@/graphql/report.gql';
@@ -43,6 +61,7 @@ import DELEGATETOEMPLOYEE from '@/graphql/DelegateToEmployee.gql';
 import FINISHREPORT from '@/graphql/FinishReport.gql';
 import * as types from '@/utils/status';
 import DatePicker from '@/components/DatePicker.vue';
+import moment from 'moment';
 
 export default {
   data() {
@@ -54,7 +73,7 @@ export default {
       openDate: false,
       date: '',
       options: {
-        zoomControl: true,
+        zoomControl: false,
         mapTypeControl: false,
         scaleControl: false,
         streetViewControl: false,
@@ -67,6 +86,21 @@ export default {
     };
   },
   computed: {
+    reportDetails() {
+      if (this.report) {
+        console.log(this.report);
+        return {
+          number: this.report.id,
+          reportDate: moment(this.report.createdAt).format('DD-MM-YYYY HH:MM'),
+          reportDeadline: this.report.deadline ? moment(this.report.deadline).format('DD-MM-YYYY HH:MM') : '-',
+          creatorImage: this.report.creator.image || 'https://clubsports.gcu.edu/wp-content/uploads/Coach-Avator.png',
+          creatorPhone: this.report.creator.phone,
+          creatorName: this.report.creator.name,
+          comments: this.report.comments.map(({ comment, createdAt, status }) => ({ comment, status: status.name, date: moment(createdAt).format('DD-MM-YYYY') })),
+        };
+      }
+      return {};
+    },
     condition() {
       return ['1', '2'].includes(this.$getRole());
     },
@@ -111,21 +145,25 @@ export default {
           name: 'Orqaga',
           show: true,
           callBack: this.back,
+          color: 'blue',
         },
         {
           name: 'Rad etish',
           show: this.getStatus === types.NEW && ['1', '2'].includes(this.$getRole()),
           callBack: this.reject,
+          color: 'red',
         },
         {
           name: 'Topshiriq',
           show: this.getStatus === types.NEW && !this.$isEmployee(),
           callBack: () => { this.open = true; },
+          color: 'green',
         },
         {
           name: 'Bajarildi',
           show: this.$isEmployee(),
           callBack: this.finish,
+          color: 'green',
         },
       ];
     },
@@ -188,3 +226,34 @@ export default {
   },
 };
 </script>
+<style scoped>
+  .new-map {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0px;
+    right: 0;
+    z-index: 0;
+  }
+  .custom-comment {
+    width: 100%;
+    min-height: 100px;
+    padding-left: 25px;
+    padding-bottom: 10px;
+    padding-right: 10px;
+    border-bottom-left-radius: 65px;
+    margin: 5px;
+    text-align: center;
+    background: lightblue;
+    box-shadow: 0 -15px 15px rgba(255, 255, 255, 0.05),
+                inset 0 -1px 1px rgba(255, 255, 255, 0.05),
+                0 15px 15px rgba(0, 0, 0, 0.3),
+                inset 0 1px 1px rgba(0, 0, 0, 0.3);
+  }
+  .paper-background {
+    z-index: 1;
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+</style>
