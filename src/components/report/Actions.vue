3cd4579e710848014bbb1ef06ca77f58745e57a1
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-06 21:09:19 +0500

    Actions component created

---
+++ src/components/report/Actions.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+    v-container
+        v-row
+            v-col(v-for="item in buttons" :cols="counts" v-if="item.show").pa-0
+                v-btn(block outlined :color="item.color" @click="item.callBack") {{ item.name }}
+</template>
+<script>
+import * as types from '@/utils/status';
+
+export default {
+  name: 'ReportActions',
+  props: {
+    status: {
+      type: Object,
+      required: true,
+    },
+    delegate: {
+      type: Function,
+      required: true,
+    },
+    reject: {
+      type: Function,
+      required: true,
+    },
+    finish: {
+      type: Function,
+      required: true,
+    },
+  },
+  computed: {
+    getStatus() {
+      return this.$reportStatus(this.status);
+    },
+    buttons() {
+      return [
+        {
+          name: 'Rad etish',
+          show: this.getStatus === types.NEW && ['1', '2'].includes(this.$getRole()),
+          callBack: this.reject,
+          color: 'red',
+        },
+        {
+          name: 'Topshiriq',
+          show: this.getStatus === types.NEW && !this.$isEmployee(),
+          callBack: this.delegate,
+          color: 'green',
+        },
+        {
+          name: 'Bajarildi',
+          show: this.$isEmployee(),
+          callBack: this.finish,
+          color: 'green',
+        },
+      ];
+    },
+    counts() {
+      const counts = this.buttons.filter(({ show }) => show).length;
+      return counts == '0' ? 12 : (12 / counts);
+    },
+  },
+};
+</script>
